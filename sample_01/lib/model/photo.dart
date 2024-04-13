e20cb8a69cddcfe4224130961628a3b872268cc4
chasenpai <wvwv0612@gmail.com>
2024-04-13 10:02:36 +0900

    freezed

--- sample_01/lib/model/photo.dart
+++ sample_01/lib/model/photo.dart
@@ -1,21 +1,36 @@
-
 import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+// part 'photo.g.dart';
+
+// @JsonSerializable()
+// class Photo extends Equatable {
+//   final int id;
+//   final String tags;
+//   @JsonKey(name: 'previewURL')
+//   final String previewUrl;
+//
+//   const Photo({required this.id, required this.tags, required this.previewUrl});
+//
+//   factory Photo.fromJson(Map<String, dynamic> json)
+//     => _$PhotoFromJson(json);
+//
+//   @override
+//   List<Object?> get props => [id];
+// }
+
+//freezed
+part 'photo.freezed.dart';
 part 'photo.g.dart';
 
-@JsonSerializable()
-class Photo extends Equatable {
-  final int id;
-  final String tags;
-  @JsonKey(name: 'previewURL')
-  final String previewUrl;
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required int id,
+    required String tags,
+    required String previewURL,
+  }) = _Photo;
 
-  const Photo({required this.id, required this.tags, required this.previewUrl});
-
-  factory Photo.fromJson(Map<String, dynamic> json)
-    => _$PhotoFromJson(json);
-
-  @override
-  List<Object?> get props => [id];
+  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
 }
